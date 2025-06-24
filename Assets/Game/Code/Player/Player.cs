1fc50ac2cd9796c4bf2d9bb97dbe1c3bd9dd3d1c
vicmac <>
2025-06-24 20:57:21 +0500

    add: animations

--- Assets/Game/Code/Player/Player.cs
+++ Assets/Game/Code/Player/Player.cs
@@ -2,11 +2,12 @@
 
 namespace SteveAdventure
 {
-    [RequireComponent(typeof(InputHandler), typeof(PlayerMover))]
+    [RequireComponent(typeof(InputHandler), typeof(PlayerMover), typeof(PlayerAnimationController))]
     public class Player : MonoBehaviour
     {
         private InputHandler _inputHandler;
         private PlayerMover _playerMover;
+        private PlayerAnimationController _playerAnimation;
 
         private void Start()
         {
@@ -15,6 +16,7 @@ namespace SteveAdventure
             _inputHandler.OnMoveInputChanged += OnMoveInputHandler;
 
             _playerMover = GetComponent<PlayerMover>();
+            _playerAnimation = GetComponent<PlayerAnimationController>();
         }
 
         private void OnDestroy()
@@ -31,6 +33,7 @@ namespace SteveAdventure
         private void OnMoveInputHandler(Vector2 moveInput)
         {
             _playerMover.Moving(moveInput);
+            _playerAnimation.MoveAnimation(moveInput);
         }
     }
 }
---
+++ Assets/Game/Code/Player/PlayerAnimationController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace SteveAdventure
+{
+    public class PlayerAnimationController : MonoBehaviour
+    {
+        private static readonly int RunXAnimation = Animator.StringToHash("RunX");
+        private static readonly int RunYAnimation = Animator.StringToHash("RunY");
+        private static readonly int Speed = Animator.StringToHash("Speed");
+
+        [SerializeField] private Animator _animator;
+
+        public void MoveAnimation(Vector2 moveInput)
+        {
+            if (moveInput.magnitude > 1f)
+                moveInput.Normalize();
+            
+            _animator.SetFloat(RunXAnimation, moveInput.x);
+            _animator.SetFloat(RunYAnimation, moveInput.y);
+            _animator.SetFloat(Speed, moveInput.magnitude);
+        }
+    }
+}
